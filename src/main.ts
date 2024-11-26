f345e2b2d67f0d152ad29cf5c77cd4a044e8c15f
Henrik Wenz <henrik.wenz@gmail.com>
2024-11-26 14:06:42 +0100

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { CkeditorPlugin } from '@ckeditor/ckeditor5-vue'
+
+createApp(App).use(CkeditorPlugin).mount('#app')
